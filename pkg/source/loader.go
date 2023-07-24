5bbb7157ed4855eafdfa11309a3bd369f65e1ed1
Mara Sophie Grosch <mgrosch@anexia-it.com>
2023-07-24 16:24:54 +0200

    linting fixes: cyclomatic complexity

--- pkg/source/loader.go
+++ pkg/source/loader.go
@@ -7,13 +7,9 @@ import (
 	"net/url"
 	"os"
 	"path"
-	"strings"
 
 	git "github.com/go-git/go-git/v5"
 	gitConfig "github.com/go-git/go-git/v5/config"
-	gitPlumbing "github.com/go-git/go-git/v5/plumbing"
-
-	"github.com/Masterminds/semver/v3"
 
 	"github.com/anexia-it/go.anx.io/pkg/markdown"
 	"github.com/anexia-it/go.anx.io/pkg/types"
@@ -83,89 +79,21 @@ func (l *Loader) loadSource(pkg *types.Package) error {
 		return fmt.Errorf("error fetching source repository: %w", err)
 	}
 
-	iter, err := repo.Tags()
+	reader, err := newRepositoryReader(repo)
 	if err != nil {
-		return fmt.Errorf("error iterating tags in local git repository: %w", err)
+		return fmt.Errorf("error reading repository metadata for package %q: %w", pkg.TargetName, err)
 	}
 
-	type tagVersion struct {
-		tag     *gitPlumbing.Reference
-		version string
-	}
-
-	tags := make([]tagVersion, 0)
-
-	err = iter.ForEach(func(tag *gitPlumbing.Reference) error {
-		var commit gitPlumbing.Hash
-		if tagObject, err := repo.TagObject(tag.Hash()); err != nil {
-			commit = tag.Hash()
-		} else {
-			commit = tagObject.Target
-		}
-
-		// first we check if we have a tree for this tag
-		if commit, err := repo.CommitObject(commit); err != nil {
-			log.Printf("Not using tag %v since we do not have a commit for it (%v)", tag.Name().Short(), err)
-			return nil
-		} else if _, err := repo.TreeObject(commit.TreeHash); err != nil {
-			log.Printf("Not using tag %v since we do not have a tree for its commit", tag.Name().Short())
-			return nil //nolint:nilerr
-		}
-
-		// parse tag as semver to filter on only release tags
-		if _, err := semver.NewVersion(strings.TrimPrefix(tag.Name().Short(), "v")); err == nil {
-			tags = append(tags, tagVersion{
-				tag:     tag,
-				version: tag.Name().Short(),
-			})
-		} else {
-			log.Printf("Not using tag %v due to error %v", tag.Name().Short(), err)
-		}
-
-		return nil
-	})
-	if err != nil {
-		return fmt.Errorf("error iterating tags in local git repository: %w", err)
-	}
-
-	fileReader := repositoryReader{
-		repository: repo,
-		versions:   make(map[string]*gitPlumbing.Reference),
-	}
-
-	if len(tags) > 0 {
-		for _, v := range tags {
-			fileReader.versions[v.version] = v.tag
-		}
-	}
-
-	branchIterator, err := repo.Branches()
-	if err != nil {
-		return fmt.Errorf("error iterating branches in local git repository: %w", err)
-	}
-
-	err = branchIterator.ForEach(func(branch *gitPlumbing.Reference) error {
-		fileReader.versions[branch.Name().Short()] = branch
-		return nil
-	})
-	if err != nil {
-		return fmt.Errorf("error iterating branches in local git repository: %w", err)
-	}
-
-	if err := fileReader.readMajorVersions(); err != nil {
-		return fmt.Errorf("failed to retrieve versions of package '%v': %w", pkg.TargetName, err)
-	}
-
-	pkg.FileReader = &fileReader
+	pkg.FileReader = reader
 
 	if pkg.Summary == "" {
-		contents, err := fileReader.ReadFile("README.md", fileReader.Versions(fileReader.MajorVersions()[0])[0])
+		contents, err := reader.ReadFile("README.md", reader.Versions(reader.MajorVersions()[0])[0])
 		if err == nil {
 			pkg.Summary = markdown.ExtractFirstHeader(contents)
 		}
 	}
 
-	log.Printf("Loaded package '%v' with %v versions", pkg.TargetName, len(fileReader.versions))
+	log.Printf("Loaded package '%v' with %v versions", pkg.TargetName, len(reader.versions))
 
 	return nil
 }
